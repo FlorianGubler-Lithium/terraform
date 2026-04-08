facf32883dbcd3ceb94cc530afafe08e2efe7aa9
Florian Gubler <gubler.florian@gmx.net>
2026-04-08 20:58:30 +0200

    fix: renaming

--- terraform/vm-configs/infra/inframgmt-001.tf
+++ terraform/vm-configs/infra/inframgmt-001.tf
@@ -1,11 +1,11 @@
 module "ansible_vm" {
   source = "../../modules/init_vm/"
 
-  vm_name   = "ansible-001"
+  vm_name   = "inframgmt-001"
   vm_id     = 3011
   vm_password = var.vm_password
-  vm_ci_userdata_file_path = "vm-configs/infra/cloud-init/ansible-001/userdata.yaml.tftpl"
-  vm_ci_networkdata_file_path = "vm-configs/infra/cloud-init/ansible-001/network.yaml.tftpl"
+  vm_ci_userdata_file_path = "vm-configs/infra/cloud-init/inframgmt-001/userdata.yaml.tftpl"
+  vm_ci_networkdata_file_path = "vm-configs/infra/cloud-init/inframgmt-001/network.yaml.tftpl"
   vm_ci_base_image_file_id = var.vm_ci_base_image_file_id
   vm_memory = 4096
   vm_cpu_cores = 2
